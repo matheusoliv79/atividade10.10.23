1abb05d7fddc08fc4e53b2f5ff4f1d344e76bfb9
matheusoliv79 <146893530+matheusoliv79@users.noreply.github.com>
2023-10-10 21:07:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector("h1").textContent = "Programação e Sistemas
+Informação"
+// Adiciona um link para o GitHub
+document.querySelector("footer").innerHTML += `
+<a
+href="https://github.com/bard/explorando-o-mundo-tecnologico">GitHub</a
+>
+`
